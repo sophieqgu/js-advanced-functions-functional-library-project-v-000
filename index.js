c1b23e7caf5920856dfe9a219ad3750fe7615006
Sophie Gu <sophieqgu@gmail.com>
2021-01-26 05:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (Array.isArray(collection)) {
+        collection.forEach( element =>
+          callback(element);
+        )
+      } else { // collection is an Object
+        for (const key in collection) {
+          if (collection.hasOwnProperty(key)) {
+            const element = collection[key];
+            callback(element);
+          }
+        }
+      }
+      return collection;
     },
 
     map: function() {
